507cc44bd41001d748f9cdc04f0594f5abe7da64
giorgi2007g <bibilashvilig13@gmail.com>
2024-08-12 12:34:27 -0700

    third commit

---
+++ Day 3/lesson3.py
@@ -0,0 +1,4 @@
+current_age = int(input("How old is she? "))
+time_travel_age = int(input("How many years does she want to time travel? "))
+
+print("  She will be ", current_age + time_travel_age, " after time travel. ")
