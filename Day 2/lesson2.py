402a9537d1b687d99d9a1bbfb5b88e1aef3bcf7f
giorgi2007g <bibilashvilig13@gmail.com>
2024-08-12 12:30:31 -0700

    second commit

---
+++ Day 2/lesson2.py
@@ -0,0 +1,10 @@
+score1 = int(input("number 1 :"))
+score2 = int(input("number 2 :"))
+score3 = int(input("number 3 :"))
+score4 = int(input("number 4 :"))
+score5 = int(input("number 5 :"))
+
+sum = score1 + score2 + score3 + score4 + score5
+Arithmetic_average = sum / 5
+
+print(Arithmetic_average)
