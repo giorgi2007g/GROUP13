c7089cbb50e6839a1ff19dd848cdb803e390fd78
giorgi2007g <bibilashvilig13@gmail.com>
2024-08-13 13:28:53 -0700

    seventh commit

---
+++ Day 7/lesson7.py
@@ -0,0 +1,14 @@
+# მომხარებელს შემოატანინეთ მშობლების ასაკი, დედის და მამის ასაკი, შემდეგ თუ დედის ასაკი მეტი იქნება 
+# მამის ასაკზედაგვიბეჭდოს რომ დედა დიდი მამაზე, თუ პირიქით მამის ასაკი მეტი იქნება დედის ასაკზე 
+# მაგ შემთხვევაში დაგვიბეჭდოს  რომ მამა დიდია დედაზე. მინიშნება დაგჭირდებათ (if)
+
+
+
+Deda = int(input("DEDA SHEIYVANE SHENI ASAKI ?: "))
+Mama = int(input("MAMA SHEIYVANE SHENI ASAKI ?: "))
+
+if Deda > Mama :
+    print("Kaci ichagreba") #Dedaa didi
+
+if Mama > Deda :
+    print("Qali ichagreba") #Mamaa didi
