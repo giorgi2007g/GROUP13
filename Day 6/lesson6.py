581511afc9bc91914861440c580d22edb182a279
giorgi2007g <bibilashvilig13@gmail.com>
2024-08-13 13:24:27 -0700

    sixth commit

---
+++ Day 6/lesson6.py
@@ -0,0 +1,12 @@
+num1 = int(input("number 1: "))
+num2 = int(input("number 2: "))
+num3 = int(input("number 3: "))
+anser1 = num1 and num2
+anserw2 = num2 or num2
+
+
+answer1 = num1 < num2
+answer2 = num2 > num3
+
+print(answer1 and answer2)
+print(answer1 or answer2)
